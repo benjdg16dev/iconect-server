3336f7f149ec9e4f30fa897abd64f999675a88e3
benjdg16dev <benjdg16dev@gmail.com>
2023-02-26 13:55:15 -0500

    [chore] added env variables

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,10 @@
-const allowedOrigins = ["http://localhost:3000"];
+require("dotenv").config();
+
+const allowedOrigins = [];
+
+if (process.env.NODE_ENV === "production")
+  allowedOrigins.push(process.env.ORIGIN_PROD);
+else if (process.env.NODE_ENV === "development")
+  allowedOrigins.push(process.env.ORIGIN_DEV);
 
 module.exports = allowedOrigins;
